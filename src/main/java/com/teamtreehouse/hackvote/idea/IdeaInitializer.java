9988125fe09338f3bd7c66a0736120173d6a4aca
Chris Ramacciotti <chris.j.rama@gmail.com>
2016-09-08 12:00:00 -0500

    Added description to projection and more data to initializer

--- src/main/java/com/teamtreehouse/hackvote/idea/IdeaInitializer.java
+++ src/main/java/com/teamtreehouse/hackvote/idea/IdeaInitializer.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
+import java.util.Arrays;
+
 import static com.teamtreehouse.hackvote.vote.Vote.Status;
 
 @Service
@@ -22,12 +24,16 @@ public class IdeaInitializer {
             return;
         }
 
-        User user = new User("user","password");
-        users.save(user);
+        User user1 = new User("user1","password");
+        User user2 = new User("user2","password");
+        User user3 = new User("user3","password");
+        users.save(Arrays.asList(user1,user2,user3));
 
-        Idea vr = new Idea(user,"VR game for learning Java","Creating a VR game for learning to code would give students a truly immersive experience into the syntax of a programming language.");
-        vr.addComment(new Comment(user,"Cool idea!"));
-        vr.vote(new Vote(vr,user,Status.UP));
+        Idea vr = new Idea(user1,"VR game for learning Java","Creating a VR game for learning to code would give students a truly immersive experience into the syntax of a programming language.");
+        vr.addComment(new Comment(user1,"Cool idea!"));
+        vr.vote(new Vote(vr,user1,Status.UP));
+        vr.vote(new Vote(vr,user2,Status.UP));
+        vr.vote(new Vote(vr,user3,Status.DOWN));
         ideas.save(vr);
     }
 }
--- src/main/java/com/teamtreehouse/hackvote/idea/IdeaProjection.java
+++ src/main/java/com/teamtreehouse/hackvote/idea/IdeaProjection.java
@@ -5,6 +5,7 @@ import org.springframework.data.rest.core.config.Projection;
 @Projection(name = "ideaSummary", types = Idea.class)
 public interface IdeaProjection {
     String getTitle();
+    String getDescription();
     String getNumComments();
     int getUpVotes();
     int getDownVotes();
