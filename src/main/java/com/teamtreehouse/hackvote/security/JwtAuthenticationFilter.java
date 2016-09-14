9210f36b3e839d58fd3882f2c4a2fa8b725f46e7
Chris Ramacciotti <chris.j.rama@gmail.com>
2016-09-14 10:52:58 -0500

    Remove unused imports

--- src/main/java/com/teamtreehouse/hackvote/security/JwtAuthenticationFilter.java
+++ src/main/java/com/teamtreehouse/hackvote/security/JwtAuthenticationFilter.java
@@ -1,6 +1,5 @@
 package com.teamtreehouse.hackvote.security;
 
-import com.teamtreehouse.hackvote.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
