266dabfe399dd04eecd54e4d4fa37f14f5415c15
Chris Ramacciotti <chris.j.rama@gmail.com>
2016-09-13 19:11:36 -0500

    Initialized timestamp in comment constructor and added @Component to VoteEventHandler

--- src/main/java/com/teamtreehouse/hackvote/comment/Comment.java
+++ src/main/java/com/teamtreehouse/hackvote/comment/Comment.java
@@ -2,6 +2,7 @@ package com.teamtreehouse.hackvote.comment;
 
 import com.teamtreehouse.hackvote.core.AbstractEntity;
 import com.teamtreehouse.hackvote.core.UserData;
+import com.teamtreehouse.hackvote.idea.Idea;
 import com.teamtreehouse.hackvote.user.User;
 
 import javax.persistence.Entity;
@@ -11,18 +12,30 @@ import java.time.LocalDateTime;
 @Entity
 public class Comment extends AbstractEntity implements UserData {
     @ManyToOne
+    private Idea idea;
+
+    @ManyToOne
     private User user;
     private LocalDateTime postedDate;
     private String comment;
 
     protected Comment() {
         super();
+        postedDate = LocalDateTime.now();
     }
 
     public Comment(User user, String comment) {
         this();
         this.user = user;
         this.comment = comment;
+    }
+
+    public Idea getIdea() {
+        return idea;
+    }
+
+    public void setIdea(Idea idea) {
+        this.idea = idea;
     }
 
     public User getUser() {
--- src/main/java/com/teamtreehouse/hackvote/vote/VoteEventHandler.java
+++ src/main/java/com/teamtreehouse/hackvote/vote/VoteEventHandler.java
@@ -2,7 +2,9 @@ package com.teamtreehouse.hackvote.vote;
 
 import com.teamtreehouse.hackvote.core.AbstractEventHandler;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.stereotype.Component;
 
+@Component
 @RepositoryEventHandler(Vote.class)
 public class VoteEventHandler extends AbstractEventHandler<Vote> {
 }
